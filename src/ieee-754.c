5ed96bbb37fb8a78a5e42474d63dccce30bac6c3
Rodrigo de Farias Gomes <shpnft@gmail.com>
2023-04-11 16:31:02 -0400

    nada demais

--- src/ieee-754.c
+++ src/ieee-754.c
@@ -1,6 +1,6 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
         double numero;
@@ -13,41 +13,39 @@ int main(int argc, char **argv) {
 
         numero = atof(argv[1]);
 
-        if (numero > 0) sinal = '0';
-        else sinal = '1';
+        if (numero > 0)
+                sinal = '0';
+        else
+                sinal = '1';
 
         numero = fabs(numero);
-        expoente=floor(log(numero)/log(2));
+        expoente = floor(log(numero) / log(2));
 
-        m=numero - pow(2,expoente);
+        m = numero - pow(2, expoente);
 
-        j=0;
-        s[0]=-1;
-        for (int i=0; i < 24; i++) {
+        j = 0;
+        s[0] = -1;
+        for (int i = 0; i < 24; i++) {
                 if (m > 0 && s[j] < 0) {
-                        e=floor(log(m)/log(2));
-                        s[j] = expoente - e -1;
-                        m = m - pow(2,e);
+                        e = floor(log(m) / log(2));
+                        s[j] = expoente - e - 1;
+                        m = m - pow(2, e);
                 }
                 if (i == s[j]) {
-                        mantissa[i]='1';
-                        s[++j]=-1;
-                } else mantissa[i]= '0';
+                        mantissa[i] = '1';
+                        s[++j] = -1;
+                } else
+                        mantissa[i] = '0';
         }
-        mantissa[24]='\0';
+        mantissa[24] = '\0';
 
-        expoente=expoente+127;
-        for (int i=7;i>=0; i--) {
-                expoente_bin[i]=expoente%2+'0';
-                expoente=expoente/2;
+        expoente = expoente + 127;
+        for (int i = 7; i >= 0; i--) {
+                expoente_bin[i] = expoente % 2 + '0';
+                expoente = expoente / 2;
         }
-        expoente_bin[8]='\0';
+        expoente_bin[8] = '\0';
 
         printf("%c%s%s\n", sinal, expoente_bin, mantissa);
         return 0;
 }
-
-
-
-
-
