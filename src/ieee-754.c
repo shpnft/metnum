45a3606c0af3470f11ce633237886502fc4477e7
Rodrigo de Farias Gomes <shpnft@gmail.com>
2023-04-12 00:17:57 -0400

    nada demais

--- src/ieee-754.c
+++ src/ieee-754.c
@@ -3,48 +3,60 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-        double numero;
-        char sinal;
-        int expoente;
-        char expoente_bin[9];
-        char mantissa[25];
-        double m;
-        int s, e;
+    double numero;
+    char sinal;
+    int expoente;
+    char expoenteBin[9];
+    char mantissa[25];
+    double m;
+    int s, e;
 
-        numero = atof(argv[1]);
+    numero = atof(argv[1]);
 
-        if (numero > 0)
-                sinal = '0';
+    if (numero > 0)
+        sinal = '0';
+    else
+        sinal = '1';
+
+    numero = fabs(numero);
+    expoente = floor(log(numero) / log(2));
+
+    m = numero - pow(2, expoente);
+    s = -1;
+    for (int i = 0; i < 24; i++) {
+        if (m > 0 && s < 0) {
+            e = floor(log(m) / log(2));
+            s = expoente - e - 1;
+            m = m - pow(2, e);
+        }
+        if (i == s) {
+            mantissa[i] = '1';
+            s = -1;
+        } else
+            mantissa[i] = '0';
+    }
+
+    if (mantissa[23] == '1') {
+        s = 22;
+        while (mantissa[s] == '1' && s >= 0)
+            s = s - 1;
+        if (s < 0)
+            expoente = expoente + 1;
         else
-                sinal = '1';
+            mantissa[s] = '1';
 
-        numero = fabs(numero);
-        expoente = floor(log(numero) / log(2));
+        for (s = s + 1; s < 23; s++)
+            mantissa[s] = '0';
+    }
+    mantissa[23] = '\0';
 
-        m = numero - pow(2, expoente);
+    expoente = expoente + 127;
+    for (int i = 7; i >= 0; i--) {
+        expoenteBin[i] = expoente % 2 + '0';
+        expoente = expoente / 2;
+    }
+    expoenteBin[8] = '\0';
 
-        s = -1;
-        for (int i = 0; i < 24; i++) {
-                if (m > 0 && s < 0) {
-                        e = floor(log(m) / log(2));
-                        s = expoente - e - 1;
-                        m = m - pow(2, e);
-                }
-                if (i == s) {
-                        mantissa[i] = '1';
-                        s = -1;
-                } else
-                        mantissa[i] = '0';
-        }
-        mantissa[24] = '\0';
-
-        expoente = expoente + 127;
-        for (int i = 7; i >= 0; i--) {
-                expoente_bin[i] = expoente % 2 + '0';
-                expoente = expoente / 2;
-        }
-        expoente_bin[8] = '\0';
-
-        printf("%c%s%s\n", sinal, expoente_bin, mantissa);
-        return 0;
+    printf("%c%s%s\n", sinal, expoenteBin, mantissa);
+    return 0;
 }
--- src/ieee-754.py
+++ src/ieee-754.py
@@ -1,35 +1,50 @@
-from math import floor,log
+from math import floor, log
 import sys
+
+mantissa = ""
 
 numero = float(sys.argv[1])
 
-if numero > 0: sinal='0'
-else: sinal='1'
+if numero > 0:
+    sinal = "0"
+else:
+    sinal = "1"
 
 numero = abs(numero)
-expoente = floor(log(numero)/log(2))
+expoente = floor(log(numero) / log(2))
 
 m = numero - 2**expoente
 
-s=-1
-mantissa=""
+s = -1
 for i in range(24):
     if m > 0 and s < 0:
-        e = floor(log(m)/log(2))
+        e = floor(log(m) / log(2))
         s = expoente - e - 1
         m = m - 2**e
     if i == s:
-        mantissa = mantissa + '1'
+        mantissa = mantissa + "1"
         s = -1
     else:
-        mantissa = mantissa + '0'
+        mantissa = mantissa + "0"
 
-expoente=expoente+127
-expoente_bin=[]
+if mantissa[23] == "1":
+    s = 22
+    while mantissa[s] == "1" and s >= 0:
+        s = s - 1
+    if s < 0:
+        expoente = expoente + 1
+        mantissa = "0"
+    else:
+        mantissa = mantissa[0:s] + "1"
+    for i in range(s + 1, 23):
+        mantissa = mantissa + "0"
+
+mantissa = mantissa[0:23]
+
+expoente = expoente + 127
+expoenteBin = ""
 for i in range(8):
-    expoente_bin.append(str(expoente%2))
+    expoenteBin = ["0", "1"][expoente % 2] + expoenteBin 
     expoente = expoente // 2
-expoente_bin.reverse()
-expoente_bin = "".join(expoente_bin)
 
-print(f"{sinal}{expoente_bin}{mantissa}")
+print(f"{sinal}{expoenteBin}{mantissa}")
